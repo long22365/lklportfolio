a5cde7483fe955a4eb6d6acd5f72e591a4edc040
long22365 <68579216+long22365@users.noreply.github.com>
2021-01-23 16:54:13 +0800

    updated

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ $(document).ready(function(){
         if(this.scrollY >20){
             $('.navbar').addClass("sticky")
         }else{
-            $('.navbar').addClass("sticky")
+            $('.navbar').removeClass("sticky")
+        }
+        if(this.scrollY >500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
         }
     })
+});
+
+$('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
 });
